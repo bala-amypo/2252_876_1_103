b4bd3c19b69658fdfbc0dce6a896a54847b8ce39
bala-amypo <bala@amypo.in>
2025-12-19 08:31:32 +0000

    User commit

--- src/main/java/com/example/demo/Models/Employee.java
+++ src/main/java/com/example/demo/Models/Employee.java
@@ -1,18 +1,1 @@
-@Entity
-public class Employee {
-@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
-
-
-@Column(unique = true, nullable = false)
-private String email;
-
-
-private String fullName;
-private String role = "STAFF";
-private String skills; // comma separated
-private Integer maxWeeklyHours;
-
-
-private LocalDateTime createdAt = LocalDateTime.now();
-}
+\
--- src/main/java/com/example/demo/Models/User.java
+++ src/main/java/com/example/demo/Models/User.java
@@ -1,4 +1,37 @@
+package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+    name = "users",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+    }
+)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // ADMIN / ANALYST
+
+    public User() {}
+
+    // getters and setters
+}
 
 
 
